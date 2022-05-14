2ad733706c898805bc0168d61edc32d50d9d735d
taewan <mytae1@naver.con>
2022-05-14 20:10:06 +0900

    change url & magnify window

--- selenium.py
+++ selenium.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome()
-driver.get("https://www.yogiyo.co.kr/mobile/#/")
+driver.get("https://www.yogiyo.co.kr/")
+
+driver.maximize_window()
+time.sleep(1)
 
