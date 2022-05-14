e67e0afc7e926e5272f305c57835cd907a2427e0
taewan <mytae1@naver.con>
2022-05-14 20:28:05 +0900

    input place

--- selenium.py
+++ selenium.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+my_place = input("지역을 입력하세요 : ")
+
 driver = webdriver.Chrome()
 driver.get("https://www.yogiyo.co.kr/")
 
@@ -9,4 +12,7 @@ time.sleep(1)
 xpath = '''//*[@id="search"]/div/form/input'''
 element = driver.find_element(By.XPATH, xpath)
 element.clear()
+time.sleep(1)
+
+element.send_keys(my_place)
 time.sleep(1)
