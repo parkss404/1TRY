c9a056bee66824149621aa55cb2431731626d1a3
KGE108 <rkdmsdl782@naver.com>
2022-05-24 00:46:37 +0900

    added main ox quiz code

--- base code.cpp
+++ base code.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+using namespace std;
 
-string Food000[] = { "짜장면","피자"};
+string Food000[] = { "짜장면","피자" };
 string Food001[] = { "국밥","김밥","냉면","덮밥","만두","샌드위치","쌀국수","파스타","햄버거" };
 string Food010[] = { "스시","내장류","보쌈","족발","치킨","회" };
 string Food100[] = { "맵떡","부대찌개","짬뽕" };
 string Food110[] = { "닭발","닭볶음탕","등갈비","매운치킨","불족발","아구찜","찜닭" };
 string Food101[] = { "마라탕","쫄면","비빔냉면" };
-string Food111[] = { "내장볶음","닭강정","쭈꾸미" };
 string Food011[] = { "돈가스","삼겹살","불고기" };
+string Food111[] = { "내장볶음","닭강정","쭈꾸미" };
+
+
+int main() {
+	char a;
+	char b;
+	char c;
+	int st = 0;
+	int nd = 0;
+	int rd = 0;
+	cout << "혼밥 메뉴를 찾으시나요?(O,X)" << endl;
+	cin >> a;
+	if (a == 'O') {
+		st + 1;
+	}
+	cout << "매운 음식을 잘 드시나요?(O,X)" << endl;
+	cin >> b;
+	if (b == 'O') {
+		nd + 1;
+	}
+	cout << "고기메뉴가 먹고싶나요?(O,X)" << endl;
+	cin >> c;
+	if (c == 'O') {
+		rd + 1;
+	}
+}
