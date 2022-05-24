64cfbb68056943971501c323ecb9ef3c1fc997d5
KGE108 <rkdmsdl782@naver.com>
2022-05-25 03:45:07 +0900

    added random extraction from the menu string and removed some checkpoint because of the error

--- base code.cpp
+++ base code.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <initializer_list>
 using std::cout; using std::cin;
 using std::string;
 using namespace std;
@@ -14,40 +15,69 @@ string Food111[] = { "내장볶음","닭강정","쭈꾸미" };
 
 
 int main() {
+
 	char a;
 	char b;
 	char c;
+	char last;
+	int len;
 	int st = 0;
 	int nd = 0;
 	int rd = 0;
-	que1:												
+
+	que1:												//체크포인트 설정
 	cout << "혼밥 메뉴를 찾으시나요?(O,X)" << endl;
 	cin >> a;
 	if (a == 'O') {
 		st + 1;
-		que2:
 		cout << "매운 음식을 잘 드시나요?(O,X)" << endl;
 	}
 	else if (a == 'X') {
 		cout << "모두 매운 음식을 잘 드시나요?(O,X)" << endl;
 	}
-	else { goto que1; };
+	else { goto que1; };								//체크포인트로 돌아가기
 	cin >> b;
 	if (b == 'O') {
 		nd + 1;
-		que3:
 		cout << "매운 고기메뉴가 먹고싶나요?(O,X)" << endl;
 	}
 	else if (b == 'X') {
 		cout << "안매운 고기메뉴가 먹고싶나요?(O,X)" << endl;
 	}
-	else { goto que2; };
 	cin >> c;
 	if (c == 'O') {
 		rd + 1;
 	}
-	else if (c == 'X') {
-		goto next;
+	if (st == 0 && nd == 0 && rd == 0) {
+		len = sizeof(Food000) / sizeof(string);
+		cout << "당신의 추천메뉴는" << Food000[rand() % len] << "입니다." << endl;
 	}
-	else { goto que3; };
+	else if (st == 0 && nd == 0 && rd == 1) {
+		len = sizeof(Food001) / sizeof(string);
+		cout << "당신의 추천메뉴는" << Food001[rand() % len] << "입니다." << endl;
+	}
+	else if (st == 0 && nd == 1 && rd == 0) {
+		len = sizeof(Food010) / sizeof(string);
+		cout << "당신의 추천메뉴는" << Food010[rand() % len] << "입니다." << endl;
+	}
+	else if (st == 1 && nd == 0 && rd == 0) {
+		len = sizeof(Food100) / sizeof(string);
+		cout << "당신의 추천메뉴는" << Food100[rand() % len] << "입니다." << endl;
+	}
+	else if (st == 0 && nd == 1 && rd == 1) {
+		len = sizeof(Food011) / sizeof(string);
+		cout << "당신의 추천메뉴는" << Food011[rand() % len] << "입니다." << endl;
+	}
+	else if (st == 1 && nd == 0 && rd == 1) {
+		len = sizeof(Food101) / sizeof(string);
+		cout << "당신의 추천메뉴는" << Food101[rand() % len] << "입니다." << endl;
+	}
+	else if (st == 1 && nd == 1 && rd == 0) {
+		len = sizeof(Food110) / sizeof(string);
+		cout << "당신의 추천메뉴는" << Food110[rand() % len] << "입니다." << endl;
+	}
+	else if (st == 1 && nd == 1 && rd == 1) {
+		len = sizeof(Food111) / sizeof(string);
+		cout << "당신의 추천메뉴는" << Food111[rand() % len] << "입니다." << endl;
+	}
 }
