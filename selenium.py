3f322833513e9284783b50152b4e27560ad0d2b5
kimtaewan22 <85469656+kimtaewan22@users.noreply.github.com>
2022-05-30 19:57:48 +0900

    add crawling in the order of distance

--- selenium.py
+++ selenium.py
@@ -74,6 +74,11 @@ elif select_mode=='3':
     click_min_price=driver.find_element(By.CSS_SELECTOR, click_min_price_selector)
     click_min_price.click()
     time.sleep(1)
+elif select_mode=='4':
+    click_distance_selector='#content > div > div.row.restaurant-list-info > div.list-option > div > select > option:nth-child(5)'
+    click_distance=driver.find_element(By.CSS_SELECTOR,click_distance_selector)
+    click_distance.click()
+    time.sleep(1)
 
 search_selector = '.btn.btn-default.ico-search1'
 search = driver.find_element(By.CSS_SELECTOR, search_selector)
