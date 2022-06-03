977f7e2f06227cc4fa6b47149f2205eb5c442e43
KGE108 <84302539+KGE108@users.noreply.github.com>
2022-06-03 17:21:14 +0900

    Update MainActivity.java
    solved the error

--- MainActivity.java
+++ MainActivity.java
@@ -2,23 +2,35 @@ package com.example.a1try;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.net.URL;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_f;
-  
+    private Button btn_1link;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn_f=findViewById(R.id.btn_f);
+        btn_1link=findViewById(R.id.btn_1link);
 
         btn_f.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent =new Intent(MainActivity.this,Question1.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_1link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(URL.("http://pf.kakao.com/_vgheb");));
                 startActivity(intent);
             }
         });
