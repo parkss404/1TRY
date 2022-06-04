16eca4128ce1c0ee64cd6745b74674e8ff13cab4
KGE108 <84302539+KGE108@users.noreply.github.com>
2022-06-04 20:40:15 +0900

    Update XXOResult.java
    added a button to get another menu

--- App/Result/XXOResult.java
+++ App/Result/XXOResult.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.a1try.R;
 
 public class XXOResult extends AppCompatActivity {
+    
+    private Button btn_221;
 
     @Override
     protected void onCreate(@Nullable Bundle saveInstanceState) {
@@ -15,11 +17,23 @@ public class XXOResult extends AppCompatActivity {
         setContentView(R.layout.activity_result221);
         
         TextView XXO=findViewById(R.id.XXO);
+        btn_221=findViewById(R.id.btn_221);
         
         String[] XXOtxt=getResources().getStringArray(R.array.XXOtxt);
         Random random=new Random();
         int n= random.nextInt(XXOtxt.length-1);
 
         XXO.setText(XXOtxt[n]);
+        
+        btn_221.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] XXOtxt=getResources().getStringArray(R.array.XXOtxt);
+                Random random=new Random();
+                int n= random.nextInt(XXOtxt.length);
+
+                XXO.setText(XXOtxt[n]);
+            }
+        });
     }
 }
