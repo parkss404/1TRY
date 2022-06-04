c21ccfc1e527c3558b4e616336867aa5edbd9c2e
kimtaewan22 <85469656+kimtaewan22@users.noreply.github.com>
2022-06-04 17:45:06 +0900

    add Url

--- dataSorting.py
+++ dataSorting.py
@@ -22,7 +22,26 @@ foodImgUrl=["장터순대국밥:https://search.pstatic.net/common/?autoRotate=tr
            "모퉁이파스타:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxODA3MDZfMTU0%2FMDAxNTMwODM2NTU3NDYx.I_-hpWuPnNwdXV-XGB5ki1how-E7aPbfMPZGnl50gTQg.lb_8l_YjBuxOGf-wkVM8hkhwYbzUDcg9k0EaC79obsEg.JPEG.azml89%2FIMG_5724.jpg",
            "버거킹:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMjA0MjdfOTEg%2FMDAxNjUxMDY2NjMyNjk3.Wx0W65HSXErlHff7Oz0RCaLiVmVaWsjRVvo6d0y-I0Eg.E5Ti_ZkcRPSFmlkoOq-D0W8XZZAWse26lpB-6-VVMZEg.JPEG%2Fupload_893d9228811e4c4d0ebaf3ebfc94ea36.jpg",
            "한가네짬뽕:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA4MjBfMTI5%2FMDAxNTk3OTI5MDYzMDU4.s5EFI1Gs7zPWDjtIKEz4PcM1bjwYkgOxd2qtnGQJ4EMg.ga7XslfOPlU7_3qV-NsP29por4Sr8Q7gdRFvpw6aHL4g.JPEG.heykzzang%2FKakaoTalk_20200820_214524819_06.jpg",
-           "아웃닭:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20220416_205%2F1650036326644MCGoW_JPEG%2Fupload_17e033ab11c59aa115f9f5b0fb81b43c.jpg"]
+           "아웃닭:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20220416_205%2F1650036326644MCGoW_JPEG%2Fupload_17e033ab11c59aa115f9f5b0fb81b43c.jpg",
+           "싱싱오징어바다:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxOTEyMTVfNTIg%2FMDAxNTc2NDA4MzIxNDE0.YjY1sepfxPzA2UZ_1pKGoMMs2YaTLVgStjd-vdlvgI8g.fNyBVUHdXSRXyFzIqbxfNoqaoxOKEVW-6vqybEWlCsgg.JPEG.92__sook%2FIMG_2872.jpg",
+           "소주신랑 보쌈부인:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20200416_234%2F1586968638971MlA33_JPEG%2Fupload_9c8e8490a3c8a68a804b3f5550043817.jpg",
+           "우리집 닭강정:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxNzAyMTVfMjMw%2FMDAxNDg3MDkyNDUxNTE1.8SYqyHGCoe-6NW6EyrwgDsYh60RZyBvEw9Ti5LbQCEAg.3UdNKcYm0XkU0-hvvc1bvVByVPtNljtEKNF0M0g2Olwg.JPEG.dh81193%2FIMG_7874.JPG",
+           "코리아닭발:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20220410_289%2F1649592619770WHnzf_JPEG%2Fupload_a891ef586f4b44045f50f23850dc2614.jpeg",
+           "안녕닭:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20211109_31%2F1636431113080LrSrz_JPEG%2Fupload_74896edb06c26e2caa4c4b08e8f2a67c.jpg",
+           "일미리금계찜닭:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA1MTBfMTAx%2FMDAxNjUyMTQ4OTQ3ODY0.-Wa33DcwNaPBkTKB4x8xZNP3W9Apc9RSo-DGoEiRoRAg.VYSt9g8-ZGvwONr0a2NE9YeVyAEYJwMCrXapfMgDsNYg.JPEG.hh0ops%2FKakaoTalk_20220509_133410053_16.jpg"
+           "쩔어떡볶이포차:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA0MDVfMTg4%2FMDAxNjQ5MDg0NTk2MDMz.PPSuNIqNhi_QGWUUDhnsLDT0uBAC4c1Syj3r4OnEgOkg.NenUnKEHL2pZphUBhLnNbfMYPk0LKef6TFzDwhMCzYAg.JPEG.qwp0923%2F20220327%25A3%25DF222702.jpg"
+           "피자웨이브:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjAyMDJfNzYg%2FMDAxNjQzNzg1OTcyODY3.qXAFw-awCNeFVcTw7ZlbnNrgpkMeAJoAu0ec5ppi95Ag.9NNRZAa01Kd6yTw2k_13j3dE4G6qsiGjHFd4J_ECk_0g.JPEG.ghkdtjswjd11%2FKakaoTalk_20220129_013858410_16.jpg",
+           "홍스족발:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxOTEwMTlfMjMy%2FMDAxNTcxNDU3NzYzMTI4.oC3ekJEVwkBK3v8THeMDv68UW9IOZ2iejzIHpEWqLr4g.nuqzi1JgL_oWJeC75RWZNkfOP-0O1p-m2NT5XSTVf4Ig.JPEG.kwiyongi%2F1571457762114.JPG",
+           "부대통령뚝배기 충북대중문점:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA3MTVfMjky%2FMDAxNTk0NzQ4ODMwOTMz.iChUJRgpmjIKQ3opw6FX1alWCOSFqHGNcO8r3dJicQcg.9QLBeqjNDCc0pGHu_i_7Xl4ylhuGFPHF0UV179kDOp8g.JPEG.instar360%2F1594748830983.jpg",
+           "바다향쭈꾸미낙지볶음:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxOTA1MDlfNCAg%2FMDAxNTU3NDExMzIzNTk3.B5P53qWInd2l3pE9izyqcwOKMVf-yUhydHOIOtt5wHEg.eWhLBCH30D6kqGV9Jyt80JHASrnUg-kfAtitMaq272gg.JPEG.pallorabbit%2F20.JPG",
+           "은하수식당:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20220321_245%2F164784083154595B7w_JPEG%2Fupload_e2caeb2872f2b4228c1f73aee4a8cc14.jpg",
+           "곱창다방:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA1MTJfMjk3%2FMDAxNjUyMzM1NTM2NDAy.apZToWVXvjX9ZCbUoj0sPw_mNucPrPs2tiZ7n5X7RNog.gHIt-fPkboZo7qP-5zIiIrKpl4jctcTSFrhChl7ZkUYg.JPEG.dlsrlrhdwb%2F20220507%25A3%25DF192353.jpg",
+           "짚신스시&롤:https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxODExMTNfMjY1%2FMDAxNTQyMTE0ODExMzM3.rZDdOkpoKmWg_ztVJOnVopP4OfmJXhv84fUu1WvP_Tkg.q9BMLpM9icx_ySvZ1QtTY2FzF7wFTZ1MwFhyYr32IVEg.JPEG.syh9158%2F1542114621991.jpg",
+           "이런이궈:",
+           "한가네짬뽕:",
+           "누구나홀딱반한닭:",
+           "등촌칼국수:",
+           "삼겹살:"]
 
 foodMapUrl=["장터순대국밥:https://map.naver.com/v5/search/%EC%9E%A5%ED%84%B0%EC%88%9C%EB%8C%80%EA%B5%AD%EB%B0%A5/place/37683659?c=14188800.0964972,4388091.7322557,17,0,0,0,dh&placePath=%3Fentry%253Dpll",
            "육쌈냉면 충북대점:https://map.naver.com/v5/entry/place/21290001?c=14188335.3376231,4387961.7934544,17,0,0,0,dh"
@@ -32,4 +51,22 @@ foodMapUrl=["장터순대국밥:https://map.naver.com/v5/search/%EC%9E%A5%ED%84%
            "버거킹:https://map.naver.com/v5/search/%EB%B2%84%EA%B1%B0%ED%82%B9?c=14188573.6726529,4388047.3140102,16,0,0,0,dh",
            "한가네짬뽕:https://map.naver.com/v5/search/%ED%95%9C%EA%B0%80%EB%84%A4%EC%A7%AC%EB%BD%95/place/37944259?c=14188355.5866385,4387981.4083276,17,0,0,0,dh&placePath=%3Fentry%253Dpll",
            "아웃닭:https://map.naver.com/v5/search/%EC%95%84%EC%9B%83%EB%8B%AD/place/36379006?c=14188269.2917692,4388092.2455220,16,0,0,0,dh",
-           "싱싱오징어바다:"]
+           "싱싱오징어바다:https://map.naver.com/v5/search/%EC%8B%B1%EC%8B%B1%EC%98%A4%EC%A7%95%EC%96%B4%EB%B0%94%EB%8B%A4/place/34188345?c=14188642.9356401,4388005.5038752,16,0,0,0,dh&placePath=%3Fentry%253Dpll",
+           "소주신랑 보쌈부인:https://map.naver.com/v5/entry/place/926739775?c=14188547.3901211,4388028.7949023,17,0,0,0,dh",
+           "우리집 닭강정:https://map.naver.com/v5/search/%EC%9A%B0%EB%A6%AC%EC%A7%91%EB%8B%AD%EA%B0%95%EC%A0%95/place/38612298?c=14188382.0138856,4388153.2966377,16,0,0,0,dh&placePath=%3Fentry%253Dpll",
+           "코리아 닭발:https://map.naver.com/v5/search/%EC%BD%94%EB%A6%AC%EC%95%84%EB%8B%AD%EB%B0%9C/place/1707433717?c=14188401.1274422,4387937.7396209,16,0,0,0,dh&placePath=%3Fentry%253Dpll",
+           "안녕닭:https://map.naver.com/v5/search/%EC%95%88%EB%85%95%EB%8B%AD/place/1460495164?c=14188402.1070537,4387987.4426133,16,0,0,0,dh&placePath=%3Fentry%253Dpll",
+           "일미리금계찜닭:https://map.naver.com/v5/search/%EC%9D%BC%EB%AF%B8%EB%A6%AC%EA%B8%88%EA%B3%84%EC%B0%9C%EB%8B%AD/place/1767731763?c=14188279.0878844,4387878.7566319,16,0,0,0,dh&placePath=%3Fentry%253Dpll",
+           "쩔어떡볶이포차:https://map.naver.com/v5/entry/place/1956461738?c=14188471.3811728,4388082.3963631,17,0,0,0,dh",
+           "피자웨이브:https://map.naver.com/v5/search/%ED%94%BC%EC%9E%90%EC%9B%A8%EC%9D%B4%EB%B8%8C/place/1437077056?c=14188472.1492773,4388006.4194238,17,0,0,0,dh&placePath=%3Fentry%253Dpll",
+           "홍스족발:https://map.naver.com/v5/search/%ED%99%8D%EC%8A%A4%EC%A1%B1%EB%B0%9C/place/1276617246?c=14188366.1953860,4387982.9481105,17,0,0,0,dh&placePath=%3Fentry%253Dpll",
+           "부대통령뚝배기 충북대중문점:https://map.naver.com/v5/entry/place/1641995762?c=14188379.9210792,4387913.0893529,17,0,0,0,dh",
+           "바다향쭈꾸미낙지볶음:https://map.naver.com/v5/entry/place/37758290?c=14188788.9422842,4388012.6479311,17,0,0,0,dh",
+           "은하수식당:https://map.naver.com/v5/search/%EC%9D%80%ED%95%98%EC%88%98%EC%8B%9D%EB%8B%B9/place/1751526576?c=14188178.6331759,4387932.6347774,16,0,0,0,dh&placePath=%3Fentry%253Dpll",
+           "곱창다방:https://map.naver.com/v5/search/%EA%B3%B1%EC%B0%BD%EB%8B%A4%EB%B0%A9/place/1786485744?c=14188371.5943813,4387871.8762292,16,0,0,0,dh&placePath=%3Fentry%253Dpll",
+           "짚신스시&롤:https://map.naver.com/v5/entry/place/1438031726?c=14188407.4169934,4387911.2998871,16,0,0,0,dh",
+           "이런이궈:",
+           "한가네짬뽕:",
+           "누구나홀딱반한닭:",
+           "등촌칼국수:",
+           "삼겹살:"]
