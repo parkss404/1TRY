25e2ab9ebd5346e7c35e3677e1eaf25c12c44bf3
KGE108 <84302539+KGE108@users.noreply.github.com>
2022-06-04 20:17:29 +0900

    Update OOOResult.java
    added a button to get another menu

--- App/Result/OOOResult.java
+++ App/Result/OOOResult.java
@@ -9,17 +9,31 @@ import com.example.a1try.R;
 
 public class OOOResult extends AppCompatActivity {
 
+    private Button btn_111;
+    
     @Override
     protected void onCreate(@Nullable Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_result111);
         
         TextView OOO=findViewById(R.id.OOO);
+        btn_111=findViewById(R.id.btn_111);
         
         String[] OOOtxt=getResources().getStringArray(R.array.OOOtxt);
         Random random=new Random();
         int n= random.nextInt(OOOtxt.length-1);
 
         OOO.setText(OOOtxt[n]);
+        
+        btn_111.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String[] OOOtxt = getResources().getStringArray(R.array.OOOtxt);
+                Random random = new Random();
+                int n = random.nextInt(OOOtxt.length);
+
+                OOO.setText(OOOtxt[n]);
+            }
+        });
     }
 }
