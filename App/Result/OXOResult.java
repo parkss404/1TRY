a785b811f0aebf58c57f405791f4303e47b99469
KGE108 <84302539+KGE108@users.noreply.github.com>
2022-06-04 19:50:57 +0900

    Update OXOResult.java
    added random function to give the random menu

--- App/Result/OXOResult.java
+++ App/Result/OXOResult.java
@@ -12,5 +12,13 @@ public class OXOResult extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_result121);
+        
+        TextView OXO=findViewById(R.id.OXO);
+        
+        String[] OXOtxt=getResources().getStringArray(R.array.OXOtxt);
+        Random random=new Random();
+        int n= random.nextInt(OXOtxt.length-1);
+
+        OXO.setText(OXOtxt[n]);
     }
 }
