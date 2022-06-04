c57e8fb5bfd94a62716a173e1ad7229a82dad17a
KGE108 <84302539+KGE108@users.noreply.github.com>
2022-06-04 21:49:30 +0900

    Update XXOResult.java

--- App/Result/XXOResult.java
+++ App/Result/XXOResult.java
@@ -62,7 +62,9 @@ public class XXOResult extends AppCompatActivity {
                         .setPositiveButton("No", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int end) {
-                                finish();
+                                moveTaskToBack(true);
+                                finishAndRemoveTask();
+                                System.exit(0);
                             }
                         });
                 AlertDialog alert=builder.create();
