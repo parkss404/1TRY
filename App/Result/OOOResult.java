3a8a99bc981e9fc34fcd7bcf7468be4e66f9a4d6
KGE108 <84302539+KGE108@users.noreply.github.com>
2022-06-04 19:47:59 +0900

    Update OOOResult.java
    added random function to give the random menu

--- App/Result/OOOResult.java
+++ App/Result/OOOResult.java
@@ -13,5 +13,13 @@ public class OOOResult extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_result111);
+        
+        TextView OOO=findViewById(R.id.OOO);
+        
+        String[] OOOtxt=getResources().getStringArray(R.array.OOOtxt);
+        Random random=new Random();
+        int n= random.nextInt(OOOtxt.length-1);
+
+        OOO.setText(OOOtxt[n]);
     }
 }
