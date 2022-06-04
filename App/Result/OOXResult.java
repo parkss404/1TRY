b4ccc2ae14c2578490fa00d692269bcdbbb2e8b3
KGE108 <84302539+KGE108@users.noreply.github.com>
2022-06-04 19:48:46 +0900

    Update OOXResult.java
    added random function to give the random menu

--- App/Result/OOXResult.java
+++ App/Result/OOXResult.java
@@ -13,5 +13,13 @@ public class OOXResult extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_result112);
+        
+        TextView OOX=findViewById(R.id.OOX);
+        
+        String[] OOXtxt=getResources().getStringArray(R.array.OOXtxt);
+        Random random=new Random();
+        int n= random.nextInt(OOXtxt.length-1);
+
+        OOX.setText(OOXtxt[n]);
     }
 }
